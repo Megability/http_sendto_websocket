2465e9ece8d2c637a155161e73405d08329f4dd8
Baisiye <adam@altamob.com>
2017-04-28 16:35:06 +0800

    swoole_table取代redis

---
+++ send_table.php
@@ -0,0 +1,13 @@
+<?php
+$post_data = array("msg"=>"msg: ".$_GET['msg']);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'http://ip:port/socket_table.php');
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$ret = curl_exec($ch);
+$curl_errno = curl_errno($ch);
+$curl_error = curl_error($ch);
+$retcode = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+curl_close($ch);
+echo $ret."\n";
---
+++ socket_table.php
@@ -0,0 +1,46 @@
+<?php
+
+$table = new swoole_table(1024);
+$table->column('fd', swoole_table::TYPE_INT);
+$table->create();
+
+$server = new swoole_websocket_server("0.0.0.0", 9502);
+$server->table = $table;
+
+$server->on('open', function (swoole_websocket_server $server, $request) {
+    //$token = $request->get['token'];echo $token;
+    echo "\nconnection open: " . $request->fd . "\n";
+    if($request->header['upgrade'] == 'websocket'){//只存储websocket-client
+        $server->table->set($request->fd, array('fd' => $request->fd));//获取客户端id插入table 
+    }
+});
+
+$server->on('message', function (swoole_websocket_server $server, $frame) {
+    /*
+    echo "\nreceive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}" . "\n";
+    foreach ($server->table as $u) {
+        $server->push($u['fd'], $frame->data );//消息广播给所有客户端
+    }
+    */
+});
+
+$server->on('close', function ($server, $fd) {
+    echo "\nconnection close: " . $fd . "\n";
+    $server->table->del($fd);//从table中删除断开的id
+    //foreach ($server->table as $u) {
+    //    var_dump($u); //输出整个table
+    //}
+});
+
+$server->on('Request', function($request, $response) use($server) {
+    echo "\nconnection open: " . $request->fd . " msg: ".$request->post["msg"] . "\n";
+    foreach ($server->table as $u) {
+        if($request->fd != $u['fd']){
+            $server->push($u['fd'], $request->post["msg"]);
+        }
+    }
+
+    $response->end("success");
+});
+
+$server->start();
